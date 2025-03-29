3205ebf578b5b121f295437d93d2426660d49583
hohlovkiril <hohlov.kiril.ua@gmail.com>
2025-03-29 14:10:12 +0200

    Update

--- src/components/SnackbarItem.tsx
+++ src/components/SnackbarItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SnackbarContent } from "@mui/material";
+import { SnackbarContent, Stack } from "@mui/material";
 import { SnackbarItem } from "../common";
 
 interface IProps {
@@ -10,8 +10,19 @@ export const SnackbarItemContainer: React.FC<IProps> = (props) => {
   
   return (
     <SnackbarContent
+    
       className={['snackbar', props.item.className].join(' ')}
-      message={props.item.message}
+      message={(
+        <Stack
+          direction='row'
+          justifyContent='flex-start'
+          alignItems='center'
+          gap={1}
+        >
+          {props.item.icon}
+          {props.item.message}
+        </Stack>
+      )}
       action={props.item.actions ? props.item.actions(props.item.id) : undefined}
       sx={{ mt: 0, mb: 0 }}
     />
